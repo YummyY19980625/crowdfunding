2ae37f383e9be1caa72f94f28b0c06b5484cbefe
YummyY19980625 <37955621+YummyY19980625@users.noreply.github.com>
2018-04-02 20:56:55 +0800

    Add files via upload

---
+++ profile_ent.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <h2>Profile</h2>
+
+    <?php
+
+    session_start();
+
+    $db = pg_connect("host=localhost port=5432 dbname=Crowdfunding user=postgres password=980412");
+
+    if(!$db){
+
+        echo "connection failed?<br>";
+
+    }
+
+    $id = $_SESSION["userid"]    
+
+    $sql1 = "select * from entrepreneurs_is where e_id = '$id'";
+
+    $result1 = pg_query($db,$sql1);
+
+    $name = pg_fetch_assoc($result1);
+
+    echo "<br>Name: $name[name] <br>";
+
+    echo "<br>Email: $name[email] <br>";
+
+    echo "<br>Address: $name[address] <br>";
+
+    ?>
+
+</body>
+
+</html>
