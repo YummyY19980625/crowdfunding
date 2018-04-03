25cdab34bba549ff78204d04a865ebf9838b6ec6
Hou Denghao <31087880+HouDenghao@users.noreply.github.com>
2018-04-03 20:53:38 +0800

    Update project history
    new version could close project history

--- projecthistory.php
+++ projecthistory.php
@@ -20,21 +20,34 @@
 			$p_id = $row['project_id'];
 			$p_name = $row['title'];
 			$p_invest = $row['current_invest'];
+			$p_state = $row['state'];
 			echo "<li>$p_name</li>";
-            echo"<table style = 'text-align:left;' border= '1'>
-                <tr><th>in_id</th><th>date</th><th>round</th><th>amount</th></tr>";
             $query2 = "select * from invest where p_id = '$p_id' ";
-            $result2 = pg_query($db,$query2);
+            $result2 = pg_query($db, $query2);
             $rownum2 = pg_num_rows($result2);
-            for($j = 1; $j < $rownum+1; $j = $j + 1){
-                $row2 = pg_fetch_assoc($result2);
-                $in_id = $row2['in_id'];
-                $date = $row2['invest_date'];
-                $amount = $row2['amount'];
-                $round = $row2['round'];
-                echo "<tr><td>$in_id</td><td>$date</td><td>$round</td><td>$amount</td></tr>";
+			if($rownum2 == 0){
+			   echo"No Investment!";	
+			}else{
+		       echo"<table style = 'text-align:left;' border= '1'>
+                 <tr><th>in_id</th><th>date</th><th>round</th><th>amount</th></tr>";
+              for($j = 1; $j < $rownum+1; $j = $j + 1){
+                 $row2 = pg_fetch_assoc($result2);
+                 $in_id = $row2['in_id'];
+                 $date = $row2['invest_date'];
+                 $amount = $row2['amount'];
+                 $round = $row2['round'];
+               echo "<tr><td>$in_id</td><td>$date</td><td>$round</td><td>$amount</td></tr>";
             }
-            echo"</table>";
+               echo"</table>";
+			}
+			if($p_state){
+			   echo"<br><input type='submit' name='new' value = 'close'/></br> ";
+			   if($_POST['new']){
+		        $query3 = "update start_project set state = false where project_id = $p_id";
+                $result3 = pg_query($db,$sql_updatep);
+			   }
+			}
+			
 		}
         ?>        
     </body>
